bfe78c1c3dc716a07b481ffbf648e974242c862b
gyanish77 <gyanish@luminoguru.com>
2022-05-13 17:46:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello Pwa</h2>
+      <h2>Hello Pwa</h2>
+      <h2>Hello Pwa</h2>
+    </div>
+  );
+}
+
+export default App;
